8e067006385488a19c60751b21e6b94ef4242f86
Ayşe Elif Öztürk <ozturk.a.elif@gmail.com>
2024-12-14 12:42:55 +0300

    ClientHandler class finished

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -57,6 +57,22 @@ public class ClientHandler implements Runnable{
     }
 
     public void removeClientHandler(){
-        
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: "+clientUsername+ " left the chat..");
     }
-}
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        removeClientHandler();
+        try{
+            if (bufferedReader != null){
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null){
+                bufferedWriter.close();
+            }
+            if (socket != null){
+                socket.close();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,49 @@
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server{
     //Client'ları tutmak ve yeni client geldiğinde yeni thread spawnlama görevini yapar
 
     private ServerSocket serverSocket; //Yeni gelen bağlantıları dinlemek için kullanılır
 
+    public Server(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer(){
+        try{
+            while(! serverSocket.isClosed()){
+
+                //serverSocket.accept(); 
+                //Yeni client gelene kadar program duracak
+                //Yeni client bağlanırsa socket objesi return edilir bu sayede client ile iletişim kurulur
+                Socket socekt = serverSocket.accept();
+                System.out.println("A new client connected!");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch(IOException a){
+
+        }
+    }
+
+    public void closeServerSocket(){
+        try{
+            if(serverSocket != null){
+                serverSocket.close();
+            }
+        } catch(IOException a){
+            a.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket  = new ServerSocket(1234); //Server, clientları bu porttan dinliyor
+        Server server = new Server(serverSocket);
+        server.startServer();   
+    }
 }
