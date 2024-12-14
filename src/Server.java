e664ec5065e44bb5289b7c8dcc76bf8a41dc3a7c
Ayşe Elif Öztürk <ozturk.a.elif@gmail.com>
2024-12-14 10:37:18 +0300

    Server and ClientHandler classes created

---
+++ src/Server.java
@@ -0,0 +1,8 @@
+import java.net.ServerSocket;
+
+public class Server{
+    //Client'ları tutmak ve yeni client geldiğinde yeni thread spawnlama görevini yapar
+
+    private ServerSocket serverSocket; //Yeni gelen bağlantıları dinlemek için kullanılır
+
+}
