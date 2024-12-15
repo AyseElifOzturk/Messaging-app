a77723cd9ab2462f7c97d150823032b98700eed7
Ayşe Elif Öztürk <ozturk.a.elif@gmail.com>
2024-12-15 15:11:00 +0300

    Revert "Add files via upload"
    This reverts commit a02eff51a448ca02bbc4bb4a8a94ef8a9d4db02e.

--- src/Server.java
+++ src/Server.java
@@ -1,54 +1,49 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class Server {
-    private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
-    private ServerSocket serverSocket;
+public class Server{
+    //Client'ları tutmak ve yeni client geldiğinde yeni thread spawnlama görevini yapar
 
-    // Constructor expects a ServerSocket
-    public Server(ServerSocket serverSocket) {
+    private ServerSocket serverSocket; //Yeni gelen bağlantıları dinlemek için kullanılır
+
+    public Server(ServerSocket serverSocket){
         this.serverSocket = serverSocket;
     }
 
-    public void startServer() {
-        try {
-            while (!serverSocket.isClosed()) {
+    public void startServer(){
+        try{
+            while(! serverSocket.isClosed()){
+
+                //serverSocket.accept(); 
+                //Yeni client gelene kadar program duracak
+                //Yeni client bağlanırsa socket objesi return edilir bu sayede client ile iletişim kurulur
                 Socket socket = serverSocket.accept();
-                LOGGER.info("A new client connected!");
+                System.out.println("A new client connected!");
+
                 ClientHandler clientHandler = new ClientHandler(socket);
+
                 Thread thread = new Thread(clientHandler);
                 thread.start();
             }
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Error while accepting client connection", e);
-            closeServerSocket();
+        } catch(IOException a){
+
         }
     }
 
-    public void closeServerSocket() {
-        try {
-            if (serverSocket != null && !serverSocket.isClosed()) {
+    public void closeServerSocket(){
+        try{
+            if(serverSocket != null){
                 serverSocket.close();
-                LOGGER.info("Server socket closed successfully.");
             }
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Error while closing server socket", e);
+        } catch(IOException a){
+            a.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        try {
-            // Create a ServerSocket with the desired port number
-            ServerSocket serverSocket = new ServerSocket(12345);
-
-            // Pass the created ServerSocket to the Server constructor
-            Server server = new Server(serverSocket);
-            server.startServer();
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Error starting server", e);
-        }
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket  = new ServerSocket(1234); //Server, clientları bu porttan dinliyor
+        Server server = new Server(serverSocket);
+        server.startServer();   
     }
 }
